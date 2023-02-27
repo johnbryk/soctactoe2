e73436af39975e252f7d256b78be557116c06c70
johnbryk <43798730+johnbryk@users.noreply.github.com>
2023-02-27 13:32:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // set up server
 const express = require('express')
 const app = express()
-app.use(express.static('public'))
+app.use(express.static('build'))
 
 const http = require('http')
 const server = http.createServer(app)
